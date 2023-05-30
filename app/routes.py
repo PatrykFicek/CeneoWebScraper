76b870029b560c97185b2083507f7a26d2ed2e0d
PatrykFicek <225972@student.uek.krakow.pl>
2023-05-30 18:24:36 +0200

    templates PF

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,27 @@
 from app import app
+from flask import Flask, render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, PatrykFicek!"
+    return render_template("index.html")
 
-@app.route('/name/', defaults = {'name': "Anon"})
-@app.route('/name/<name>')
-def name(name):
-    return f"Hello, {name}"
+@app.route('/extract')
+def extract():
+    return render_template("extract.html")
+
+@app.route('/products')
+def products():
+    return render_template("products.html")
+
+@app.route('/author')
+def author():
+    return render_template("author.html")
+
+@app.route('/product1')
+def product1():
+    return render_template("product1.html")
+
+@app.route('/product2')
+def product2():
+    return render_template("product2.html")
