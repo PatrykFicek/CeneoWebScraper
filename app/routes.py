16250d64343b2d4ad39893112a9861fd8f7c22bc
PatrykFicek <225972@student.uek.krakow.pl>
2023-06-07 12:56:34 +0200

    07.06.2023 PF

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import Flask, render_template
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
@@ -18,10 +18,6 @@ def products():
 def author():
     return render_template("author.html")
 
-@app.route('/product1')
-def product1():
-    return render_template("product1.html")
-
-@app.route('/product2')
-def product2():
-    return render_template("product2.html")
+@app.route('/product')
+def product():
+    return render_template("product.html")
